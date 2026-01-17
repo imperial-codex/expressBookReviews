3a49be8d08a2d874ea26166406ea7912ee41967c
Z DEV <zindin20naim05@gmail.com>
2026-01-17 11:19:16 -0500

    implemented and tested title search

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -51,14 +51,36 @@ public_users.get('/author/:author',function (req, res) {
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const title = req.params.title.toLowerCase();
+    books = [];
+    for (const isbn in booksJS)//loop to add all books with the coresponding title the list
+    {
+      if(booksJS[isbn].title.toLowerCase() == title)
+       {
+          books.push( booksJS[isbn] ) 
+      }
+    }
+  
+    if (books.length>0){
+      return res.status(200).send(JSON.stringify(books,null,4)) //send the list of books as string if any where found 
+    }
+  else{
+    return res.status(404).json({message: title + " not in list"});}
+  
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const isbn  = req.params.isbn;
+    const review = booksJS[isbn].review;
+      
+    if (review){
+      return res.status(200).send(JSON.stringify(review ,null,4)) //send the review as string if any where found 
+    }
+  else{
+    return res.status(404).json({message:  + " not found"});}
 });
+  
+
 
 module.exports.general = public_users;
